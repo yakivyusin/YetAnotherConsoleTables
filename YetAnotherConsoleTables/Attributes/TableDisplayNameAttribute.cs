7e64385d3faf95cda03c1f40f48fce37a48725ca
Yakiv Yusin <yusin.yakiv@gmail.com>
2018-03-09 15:44:00 +0200

    Added Attributes

---
+++ YetAnotherConsoleTables/Attributes/TableDisplayNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YetAnotherConsoleTables.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field,
+        AllowMultiple = false,
+        Inherited = true)]
+    public class TableDisplayNameAttribute : Attribute
+    {
+        private string name;
+        
+        public string Name => name != null ? name : "";
+
+        public TableDisplayNameAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ YetAnotherConsoleTables/Attributes/TableIgnoreAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YetAnotherConsoleTables.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field,
+        AllowMultiple = false,
+        Inherited = true)]
+    public class TableIgnoreAttribute : Attribute
+    {
+    }
+}
