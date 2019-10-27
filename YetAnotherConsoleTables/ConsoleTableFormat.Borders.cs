54af74887e709546c108b74a7e30f32964ecbf1a
yakivyusin <yusin.yakiv@gmail.com>
2019-10-27 18:12:56 +0200

    Aded Console Tble Format Borders Flags

---
+++ YetAnotherConsoleTables/ConsoleTableFormat.Borders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace YetAnotherConsoleTables
+{
+    /// <summary>
+    /// Represents ConsoleTable output format.
+    /// </summary>
+    public partial class ConsoleTableFormat
+    {
+        [Flags]
+        public enum Borders
+        {
+            None = 0,
+            Top = 1,
+            Bottom = 2,
+            Left = 4,
+            Right = 8,
+            HeaderDelimiter = 16,
+            RowDelimiter = 32,
+            All = 63
+        }
+    }
+}
