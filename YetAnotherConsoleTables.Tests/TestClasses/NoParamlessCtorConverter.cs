35051cb9c41b7bfbf62aa936a3cec159a33b66dd
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-06-03 16:24:45 +0300

    Add Tests on Table Member Converter with Params

--- YetAnotherConsoleTables.Tests/TestClasses/NoParamlessCtorConverter.cs
+++ YetAnotherConsoleTables.Tests/TestClasses/NoParamlessCtorConverter.cs
@@ -4,11 +4,36 @@ namespace YetAnotherConsoleTables.Tests.TestClasses
 {
     internal class NoParamlessCtorConverter
     {
+        private NoParamlessCtorConverter() { }
+
+        internal class WithoutParam
+        {
 #if NET7_0_OR_GREATER
-        [TableMemberConverter<TestConverters.NoParamlessCtorConverter>]
+            [TableMemberConverter<TestConverters.NoParamlessCtorConverter>]
 #else
-        [TableMemberConverter(typeof(TestConverters.NoParamlessCtorConverter))]
+            [TableMemberConverter(typeof(TestConverters.NoParamlessCtorConverter))]
 #endif
-        public string A = "A";
+            public string A = "A";
+        }
+
+        internal class WithParam
+        {
+#if NET7_0_OR_GREATER
+            [TableMemberConverter<TestConverters.NoParamlessCtorConverter>(ConstructorParams = new object[] { 5 })]
+#else
+            [TableMemberConverter(typeof(TestConverters.NoParamlessCtorConverter), ConstructorParams = new object[] { 5 })]
+#endif
+            public string A = "A";
+        }
+
+        internal class WithParamOfWrongType
+        {
+#if NET7_0_OR_GREATER
+            [TableMemberConverter<TestConverters.NoParamlessCtorConverter>(ConstructorParams = new object[] { "a" })]
+#else
+            [TableMemberConverter(typeof(TestConverters.NoParamlessCtorConverter), ConstructorParams = new object[] { "a" })]
+#endif
+            public string A = "A";
+        }
     }
 }
